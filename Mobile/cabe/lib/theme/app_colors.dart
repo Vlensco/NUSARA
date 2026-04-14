0e6ccfa5db6da4e282ece8ec6b72448a0ad1c496
fauzanhadi44 <fauzanhadi097@gmail.com>
2026-04-14 14:05:22 +0700

    inisialisai projek

---
+++ Mobile/cabe/lib/theme/app_colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // --- PRIMARY BLUES ---
+  static const Color blue100 = Color(0xFFECF1FF);
+  static const Color blue200 = Color(0xFFC2D3FF);
+  static const Color blue300 = Color(0xFF81ACFF);
+  static const Color blue500 = Color(0xFF0086FB); // Base Primary
+  static const Color blue600 = Color(0xFF0062BB);
+  static const Color blue800 = Color(0xFF00417F);
+  static const Color blue900 = Color(0xFF002248);
+
+  // --- NEUTRAL GRAYS ---
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color gray200 = Color(0xFFD6D6D6);
+  static const Color gray300 = Color(0xFFAFAFAF);
+  static const Color gray400 = Color(0xFF898989);
+  static const Color gray500 = Color(0xFF656565);
+  static const Color gray700 = Color(0xFF434343);
+  static const Color gray900 = Color(0xFF242424);
+
+  // --- COOL GRAYS ---
+  static const Color coolGray100 = Color(0xFFF0F1F1);
+  static const Color coolGray200 = Color(0xFFD2D3D6);
+  static const Color coolGray300 = Color(0xFFA9ACB1);
+  static const Color coolGray400 = Color(0xFF83878D);
+  static const Color coolGray500 = Color(0xFF606367);
+  static const Color coolGray700 = Color(0xFF3F4144);
+  static const Color coolGray900 = Color(0xFF212224);
+}
---
+++ Mobile/cabe/lib/views/splash_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import '../theme/app_colors.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+    const SplashScreen({super.key});
+
+    @override
+    State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+    late AnimationController _controller;
+    late Animation<Color?> _colorAnimation;
+
+    @override
+    void initState() {
+        super.initState();
+
+        _controller = AnimationController(
+            duration: const Duration(seconds: 5),
+            vsync: this,
+        );
+
+        _colorAnimation = ColorTween(
+            begin: AppColors.blue200,
+            end: AppColors.white,
+        ).animate(CurvedAnimation(
+            parent: _controller,
+            curve: Curves.easeInOut,
+        ));
+
+        _controller.forward();
+
+        Timer(const Duration(seconds: 3), () {
+            if (mounted) {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (_) => const HomeScreen()),
+                );
+            }
+        });
+    }
+
+    @override
+    void dispose() {
+        _controller.dispose();
+        super.dispose();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return AnimatedBuilder(
+            animation: _colorAnimation,
+            builder: (context, child) {
+                return Scaffold(
+                    backgroundColor: _colorAnimation.value,
+                    body: Center(
+                        child: Image.asset(
+                            'assets/logo_cabe.png',
+                            width: 320,
+                            height: 320,
+                            fit: BoxFit.contain,
+                        ),
+                    ),
+                );
+            },
+        );
+    }
+}
